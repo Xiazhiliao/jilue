ab2704b50ab7dedd1f18637658c8689c980279af
Zhi_liao <671510496@qq.com>
2025-08-13 12:49:47 +0800

    【招降】fix2
    修复SR曹操【招降】在查看武将介绍后，选项错乱的问题；修复ai错误导致选择不存在选项的问题

--- character/jlsg_sr.js
+++ character/jlsg_sr.js
@@ -4530,6 +4530,11 @@ export default {
 				return event.player.countGainableCards(player, "h");
 			},
 			async cost(event, trigger, player) {
+				const upgrade = _status._jlsgsr_upgrade?.[player.playerid]?.["jlsgsr_caocao"]?.[2];
+				const storage = player.getStorage(
+					event.skill,
+					Array.from({ length: upgrade ? 4 : 3 }, () => true)
+				);
 				event.result = await player
 					.gainPlayerCard(get.prompt2("jlsg_zhaoxiang", trigger.player), trigger.player, "h")
 					.set("ai", button => {
@@ -4540,8 +4545,8 @@ export default {
 						"check",
 						(function () {
 							const gainEff = get.effect(trigger.player, { name: "shunshou_copy2" }, player, player),
-								resultList = lib.skill.jlsg_zhaoxiang.getCheck(trigger, player, _status._jlsgsr_upgrade?.[player.playerid]?.["jlsgsr_caocao"]?.[2]);
-							return resultList.some(i => i + gainEff > 0);
+								resultList = lib.skill.jlsg_zhaoxiang.getCheck(trigger, player, upgrade);
+							return resultList.some((v, i) => storage[i] && v + gainEff > 0);
 						})()
 					)
 					.set("logSkill", ["jlsg_zhaoxiang", trigger.player])
@@ -4550,25 +4555,23 @@ export default {
 				if (event.result?.bool) {
 					event.result.skill_popup = false;
 					event.result.targets = [trigger.player];
+					event.result.cost_data = { storage, upgrade };
 				}
 			},
 			async content(event, trigger, player) {
 				const {
 					cards,
 					targets: [target],
+					cost_data: { storage, upgrade },
 				} = event;
 				await player.gain(cards, target).set("ainimate", false);
 				const choiceList = ["令此【杀】不能被响应", "令此【杀】无效", "将此【杀】的目标改为你"],
-					choiced = player.getStorage(event.name, [true, true, true]),
 					list = [];
-				if (_status._jlsgsr_upgrade?.[player.playerid]?.["jlsgsr_caocao"]?.[2]) {
+				if (upgrade) {
 					choiceList.push("令目标角色于此【杀】结算后回复1点体力");
-					if (choiced.length < 4) {
-						choiced.push(true);
-					}
 				}
-				for (let i in choiced) {
-					if (!choiced[i]) {
+				for (let i in storage) {
+					if (!storage[i]) {
 						choiceList[i] = '<span style="opacity:0.5">' + choiceList[i] + "</span>";
 					} else {
 						list.add(`选项${get.cnNumber(Number(i) + 1, true)}`);
@@ -4582,32 +4585,35 @@ export default {
 					.set(
 						"choice",
 						(function () {
-							const resultList = lib.skill.jlsg_zhaoxiang.getCheck(trigger, player, _status._jlsgsr_upgrade?.[player.playerid]?.["jlsgsr_caocao"]?.[2]);
+							const resultList = lib.skill.jlsg_zhaoxiang
+									.getCheck(trigger, player, storage.length == 4)
+									.map((v, i) => (storage[i] ? v : null))
+									.filter(i => i !== null),
+								choiceList = storage.filter(i => i);
 							let max = 0,
 								choice = 0;
-							for (let i in choiced) {
-								if (choiced[i]) {
-									if (resultList[i] > max) {
-										max = resultList[i];
-										choice = i;
-									}
+							for (let i in choiceList) {
+								if (resultList[i] > max) {
+									max = resultList[i];
+									choice = i;
 								}
 							}
-							return `选项${get.cnNumber(choice + 1, true)}`;
+							return Number(choice);
 						})()
 					);
 				if (result) {
 					game.log(player, "选择了", result.control);
 					let map = ["选项一", "选项二", "选项三", "选项四"];
 					let num = map.indexOf(result.control);
-					choiced[num] = false;
-					if (!choiced.filter(i => i).length) {
-						for (let i in choiced) {
-							choiced[i] = true;
+					storage[num] = false;
+					if (storage.every(i => i === false)) {
+						for (let i in storage) {
+							storage[i] = true;
 						}
 					}
-					player.setStorage(event.name, choiced);
+					player.setStorage(event.name, storage);
 				}
+				await game.delay();
 				if (result?.control == "选项一") {
 					game.log(player, "令", trigger.card, "不能被响应");
 					trigger.getParent().directHit.addArray(game.players);
@@ -4616,10 +4622,6 @@ export default {
 					trigger.getParent().all_excluded = true;
 				} else if (result?.control == "选项三") {
 					game.log(player, "将", trigger.card, "的目标", trigger.target, "改为", player);
-					trigger.targets.remove(trigger.target);
-					trigger.targets.add(player);
-					trigger.getParent().triggeredTargets1.remove(trigger.target);
-					trigger.getParent().triggeredTargets1.add(player);
 					trigger.getParent().targets.remove(trigger.target);
 					trigger.getParent().targets.add(player);
 				} else if (result?.control == "选项四") {
@@ -5205,7 +5207,9 @@ export default {
 				list = ["1．令此【杀】不能被响应", "2．令此【杀】无效", "3．将此【杀】的目标改为你"];
 			if (upgrade["jlsgsr_caocao"]?.[2] || player?.index) {
 				list.push("4．令目标角色于此【杀】结算后回复1点体力");
-				storage.push(true);
+				if (storage.length < 4) {
+					storage.push(true);
+				}
 			}
 			for (let i in storage) {
 				if (!storage[i]) {
